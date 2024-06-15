8f238f2f42c565045f455bfeffb5da71101ca3c3
Taras Tereschenko <tarrrasio@gmail.com>
2024-06-15 16:41:03 +0500

    feat: set up lambdas and api gateway

--- lib/nodejs-aws-shop-backend-stack.ts
+++ lib/nodejs-aws-shop-backend-stack.ts
@@ -1,16 +1,65 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+	Stack,
+	type StackProps,
+	aws_apigateway,
+	aws_lambda,
+} from "aws-cdk-lib";
+import type { Construct } from "constructs";
 
-export class NodejsAwsShopBackendStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { products } from "../mocks/products";
+
+export class NodejsAwsShopBackendStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
-		// The code that defines your stack goes here
+		const getProductsListLambda = new aws_lambda.Function(
+			this,
+			"get-products-list-lambda",
+			{
+				runtime: aws_lambda.Runtime.NODEJS_20_X,
+				code: aws_lambda.Code.fromAsset("lambdas"),
+				handler: "getProductsList.getProductsList",
+				environment: {
+					PRODUCTS: JSON.stringify(products),
+				},
+			},
+		);
 
-		// example resource
-		// const queue = new sqs.Queue(this, 'NodejsAwsShopBackendQueue', {
-		//   visibilityTimeout: cdk.Duration.seconds(300)
-		// });
+		const getProductByIdLambda = new aws_lambda.Function(
+			this,
+			"get-product-by-id-lambda",
+			{
+				runtime: aws_lambda.Runtime.NODEJS_20_X,
+				code: aws_lambda.Code.fromAsset("lambdas"),
+				handler: "getProductById.getProductById",
+				environment: {
+					PRODUCTS: JSON.stringify(products),
+				},
+			},
+		);
+
+		const api = new aws_apigateway.RestApi(
+			this,
+			"nodejs-aws-shop-backend-api",
+			{
+				defaultCorsPreflightOptions: {
+					allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
+					allowMethods: aws_apigateway.Cors.ALL_METHODS,
+					allowHeaders: ["*"],
+				},
+			},
+		);
+
+		const productsResource = api.root.addResource("products");
+		productsResource.addMethod(
+			"GET",
+			new aws_apigateway.LambdaIntegration(getProductsListLambda),
+		);
+
+		const productByIdResource = productsResource.addResource("{id}");
+		productByIdResource.addMethod(
+			"GET",
+			new aws_apigateway.LambdaIntegration(getProductByIdLambda),
+		);
 	}
 }
