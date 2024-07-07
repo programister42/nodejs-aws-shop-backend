a432ee11e18418075aa6936ade093c595149de5c
Taras Tereschenko <tarrrasio@gmail.com>
2024-07-07 21:27:12 +0500

    feat: add necessary dependencies

---
+++ shared/constants.ts
@@ -0,0 +1,15 @@
+export const PRODUCT_TABLE = {
+	id: "NodejsAWSShopProductTable",
+	name: "NodejsAWSShopProductTable",
+};
+
+export const STOCK_TABLE = {
+	id: "NodejsAWSShopStockTable",
+	name: "NodejsAWSShopStockTable",
+};
+
+export const SNS_SUBSCRIPTION_EMAIL =
+	"tarrrasio+aws_sns_subscription@gmail.com";
+
+export const SNS_HIGH_PRICE_SUBSCRIPTION_EMAIL =
+	"tarrrasio+aws_sns_high_price_subscription@gmail.com";
